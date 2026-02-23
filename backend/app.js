8fff61314898e31a83d696f092c5b6b29250355e
ramizrajamondal <ramizm036@gmail.com>
2026-02-23 17:42:06 +0530

    Update CORS origin to production URL

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://apexresume-project-1.onrender.com/",
     credentials: true
 }))
 
